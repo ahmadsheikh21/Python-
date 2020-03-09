96db210e02cae3f251f53815f8ead0c4bb777e11
ahmadsheikh21 <37517939+ahmadsheikh21@users.noreply.github.com>
2020-03-09 17:27:28 +0500

    Guessing game using while loop

---
+++ Guessing game using while loop.py
@@ -0,0 +1,18 @@
+import random
+n = 20
+to_be_guessed = int(n * random.random()) + 1
+guess = 0
+while guess != to_be_guessed:
+    guess = int(input("Guess the Number between 20 :"))
+    if guess > 0:
+        if guess > to_be_guessed:
+            print("Number is larger than the Original Number")
+        elif guess < to_be_guessed:
+            print("Number is smaller than the original number")
+    else:
+             print("Sorry You Lost")
+             break
+
+else:
+        print("Congratulation you won ")
+
