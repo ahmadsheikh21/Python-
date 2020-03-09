7b71043505ca2a1a303e4ed96d84fca78438a52a
ahmadsheikh21 <37517939+ahmadsheikh21@users.noreply.github.com>
2020-03-10 00:40:54 +0500

    Pandas Basic conversion of list into Data frame

--- Pandas Basic.py
+++ Pandas Basic.py
@@ -3,3 +3,9 @@ import pandas as pd
 Webtraffic = {'Day':[1,2,3,4,5,6,7], "Visitors": [100,300,500, 800,1200,430,5543], 'BounceRate':[35,33,55,64,21,56,32]}
 df = pd.DataFrame(Webtraffic)
 print(df)
+
+print(df.head(2))
+# this will show the first two rows
+
+print(df.tail(2))
+# this will show the last two rows
